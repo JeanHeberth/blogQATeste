90934a4c653f1dcba63f01f810b9be7648756538
jeanheberth <jeanheberth19@gmail.com>
2025-10-30 19:20:41 -0300

    Initial commit

---
+++ src/main/java/br/com/blogqateste/BlogQaTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.blogqateste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogQaTesteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogQaTesteApplication.class, args);
+    }
+
+}
