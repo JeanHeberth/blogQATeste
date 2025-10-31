dda13094825b6cb98579b13c91a050c3fc1f3a13
jeanheberth <jeanheberth19@gmail.com>
2025-10-31 14:12:21 -0300

    refatorado métodos para usarem o fromEntity

--- src/main/java/br/com/blogqateste/service/PostService.java
+++ src/main/java/br/com/blogqateste/service/PostService.java
@@ -1,9 +1,10 @@
 package br.com.blogqateste.service;
 
 import br.com.blogqateste.dto.PostRequestDTO;
+import br.com.blogqateste.dto.PostResponseDTO;
 import br.com.blogqateste.entity.Post;
-import br.com.blogqateste.exception.PostNotFoundException;
 import br.com.blogqateste.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -12,39 +13,39 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository repository;
 
-    public PostService(PostRepository repository) {
-        this.repository = repository;
-    }
 
     @Transactional
-    public Post criar(PostRequestDTO dto) {
-        var post = new Post();
+    public PostResponseDTO criar(PostRequestDTO dto) {
+        Post post = new Post();
         aplicarCriacao(dto, post);
-        return repository.save(post);
+       Post postCriado = repository.save(post);
+       return PostResponseDTO.fromEntity(postCriado);
+
     }
 
     @Transactional
-    public Post atualizar(String id, PostRequestDTO dto) {
+    public PostResponseDTO atualizar(String id, PostRequestDTO dto) {
         Post post = repository.findById(id).orElseThrow();
         aplicarAtualizacao(dto, post);
-        return repository.save(post);
+        Post postCriado = repository.save(post);
+        return PostResponseDTO.fromEntity(postCriado);
     }
 
     @Transactional(readOnly = true)
-    public Page<Post> buscarTodos(Pageable pageable) {
-        return repository.findAll(pageable);
+    public Page<PostResponseDTO> buscarTodos(Pageable pageable) {
+        return repository.findAll(pageable).map(PostResponseDTO::fromEntity);
     }
 
     @Transactional(readOnly = true)
-    public Post buscarPorId(String id) {
-        return repository.findById(id).orElseThrow();
+    public PostResponseDTO buscarPorId(String id) {
+        return PostResponseDTO.fromEntity(repository.findById(id).orElseThrow());
     }
 
-    /* Helpers ------------------------------------------------------------- */
 
     private static void aplicarCriacao(PostRequestDTO dto, Post post) {
         post.setTitulo(dto.titulo());
