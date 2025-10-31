af27eeb5aca5f99233060187729766bfda99a95b
jeanheberth <jeanheberth19@gmail.com>
2025-10-31 12:40:46 -0300

    Corrigndo testes

--- src/main/java/br/com/blogqateste/repository/PostRepository.java
+++ src/main/java/br/com/blogqateste/repository/PostRepository.java
@@ -2,8 +2,6 @@ package br.com.blogqateste.repository;
 
 import br.com.blogqateste.entity.Post;
 import br.com.blogqateste.enums.TipoQa;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
@@ -12,16 +10,4 @@ public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByTipoQa(TipoQa tipoQa);
 
     List<Post> findByPublicadoTrue();
-
-    List<Post> findByAutorContainingIgnoreCase(String autor);
-
-    Page<Post> findByTipoQaAndPublicadoTrue(TipoQa tipoQa, Pageable pageable);
-
-    long countByPublicadoTrue();
-
-    long countByTipoQa(TipoQa tipoQa);
-
-    Page<Post> findByAutor(String autor, Pageable pageable);
-
-    Page<Post> findByAutorAndPublicadoTrue(String autor, Pageable pageable);
 }
