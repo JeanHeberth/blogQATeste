148039f37b6d34ba4b71cdc284db31440900da86
jeanheberth <jeanheberth19@gmail.com>
2025-11-03 19:55:20 -0300

    csasa

--- src/test/java/br/com/blogqateste/integration/config/AbstractIntegrationTest.java
+++ src/test/java/br/com/blogqateste/integration/config/AbstractIntegrationTest.java
@@ -19,6 +19,7 @@ public class AbstractIntegrationTest {
         mongoDBContainer.start();
     }
 
+    
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
