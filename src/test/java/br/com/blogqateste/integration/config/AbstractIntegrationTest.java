57b90da92af5e51e6284135d910c3091fc3d1502
Jean Heberth Souza Vieira <jeanheberth19@gmail.com>
2025-11-03 20:34:58 -0300

    Merge testes → develop (#48)
    * Testando nova atuomacao
    
    * adfs
    
    * safasdfaf

--- src/test/java/br/com/blogqateste/integration/config/AbstractIntegrationTest.java
+++ src/test/java/br/com/blogqateste/integration/config/AbstractIntegrationTest.java
@@ -19,6 +19,8 @@ public class AbstractIntegrationTest {
         mongoDBContainer.start();
     }
 
+
+
     
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
