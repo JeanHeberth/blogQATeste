2699632a20b08b4a98d4fab20f271961bf7fe6b9
jeanheberth <jeanheberth19@gmail.com>
2025-12-23 18:09:58 -0300

    alterando Jenkinsfile

---
+++ src/test/java/br/com/blogqateste/integration/CategoriaRepositoryTest.java
@@ -0,0 +1,29 @@
+package br.com.blogqateste.integration;
+
+import br.com.blogqateste.config.AbstractIntegrationTest;
+import br.com.blogqateste.entity.Categoria;
+import br.com.blogqateste.repository.CategoriaRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataMongoTest
+@Import(AbstractIntegrationTest.class)
+class CategoriaRepositoryTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    @Test
+    void deveSalvarCategoriaComSucesso() {
+        Categoria categoria = new Categoria(null, "Automação", "Testes automatizados");
+        Categoria salva = repository.save(categoria);
+
+        assertThat(salva.getId()).isNotNull();
+        assertThat(repository.existsByNome("Automação")).isTrue();
+    }
+}
+
